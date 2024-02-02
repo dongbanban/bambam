cc60c47f87db9d79b9393ddea44834097ba94714
bambam <215387880@qq.com>
2024-02-02 16:42:56 +0800

    feature: jackie long demo

--- src/views/menuDemo.jsx
+++ src/views/menuDemo.jsx
@@ -11,37 +11,25 @@ function MenuTest() {
         <Flex className='left' gap={0} horizontal>
             <Flex gap={0} vertical>
                 <Flex className='item' justify='center' align='center'>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item isFirst'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
-                    <div className='border-bottom' ></div>
+                    <div className='menu-item'>111</div>
                 </Flex>
                 <Flex className='item' justify='center' align='center'>
-                    <div className='border-top'></div>
-                    <div>111</div>
+                    <div className='menu-item isLast'>111</div>
                 </Flex>
             </Flex>
             <Flex className='right' justify='center' align='center' flex={1}>222</Flex>
