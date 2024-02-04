42563fb0015db8682bb6f9a9a13d9497b1b4cd67
bambam <215387880@qq.com>
2024-02-04 14:11:38 +0800

    feature: update border-radius

--- src/views/menuDemo.jsx
+++ src/views/menuDemo.jsx
@@ -2,35 +2,23 @@
  * @FilePath: /Users/i104/bambam/src/views/menuDemo.jsx
  * @author: dongyang(yang.dong@derbysoft.net)
  */
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { Flex } from 'antd'
 import './menuDemo.less'
 
 function MenuTest() {
+    const [menuKey, setKey] = useState(null)
+    const menuEl = useMemo(() => {
+        return new Array(7).fill('menu').map((item, index) => {
+            return <Flex className={`item${index === menuKey ? ' active-item' : ''}`} justify='center' align='center' onClick={() => setKey(index)}>
+                <div className={`menu-item${index === menuKey ? ' active-menu' : ''}${index === 0 ? ' isFirst' : ''}${index === 6 ? ' isLast' : ''}`}>{item + ' ' + index}</div>
+            </Flex>
+        })
+    }, [menuKey])
     return (
         <Flex className='left' gap={0} horizontal>
             <Flex gap={0} vertical>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item isFirst'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item'>111</div>
-                </Flex>
-                <Flex className='item' justify='center' align='center'>
-                    <div className='menu-item isLast'>111</div>
-                </Flex>
+                {menuEl}
             </Flex>
             <Flex className='right' justify='center' align='center' flex={1}>222</Flex>
         </Flex>
