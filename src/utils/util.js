ddf1fc200225e3d3050ac0145ba60fb6181da7ab
bambam <215387880@qq.com>
2024-05-13 15:40:43 +0800

    chore: remove blank line

--- src/utils/util.js
+++ src/utils/util.js
@@ -7,14 +7,20 @@ import { ValueType } from "enum";
 const getValueType = (value) => Object.prototype.toString.call(value);
 
 const isArray = (value) => getValueType(value) === ValueType.ARRAY;
+
 const isObject = (value) => getValueType(value) === ValueType.OBJECT;
+
 const isBlob = (value) => getValueType(value) === ValueType.BLOB;
+
 const isFile = (value) => getValueType(value) === ValueType.FILE;
+
 const isBlank = (value) => value === "";
+
 const isFalseValue = (value) =>
   isBlank(value) ||
   [ValueType.NULL, ValueType.UNDEFINED].includes(getValueType(value)) ||
   value === false;
+
 const getUuid = () => {
   let uuid = "";
   for (let i = 1; i <= 32; i++) {
@@ -24,15 +30,14 @@ const getUuid = () => {
   }
   return uuid;
 };
+
 const formatSizeUnits = (size) => {
   const units = ["B", "KB", "MB", "GB", "TB", "PB"];
   let unitIndex = 0;
-
   while (size >= 1024 && unitIndex < units.length - 1) {
     size /= 1024;
     unitIndex++;
   }
-
   return !!size ? `${size.toFixed(2)} ${units[unitIndex]}` : "0 B";
 };
 
