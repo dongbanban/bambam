e5fe1f199b98bc13f80cb33a6e5dfdee451cc83b
bambam <215387880@qq.com>
2024-05-21 17:19:56 +0800

    feat： add css demo

--- src/views/cssDemo.jsx
+++ src/views/cssDemo.jsx
@@ -1,8 +1,13 @@
+/*
+ * @FilePath: /src/views/cssDemo.jsx
+ * @author: dongyang
+ */
 import React from "react";
 import BackdropFilter from "./cssDemo/backdropFilter";
 import Grid from "./cssDemo/grid";
 import LightCard from "./cssDemo/lightCard";
 import CubicBezier from "./cssDemo/cubicBezier";
+import ConicGradient from "./cssDemo/conicGradient";
 
 const CssDemo = () => {
   return (
@@ -11,6 +16,7 @@ const CssDemo = () => {
       <Grid />
       <LightCard />
       <CubicBezier />
+      <ConicGradient />
     </div>
   );
 };
---
+++ src/views/cssDemo/conicGradient.jsx
@@ -0,0 +1,49 @@
+/*
+ * @FilePath: /src/views/cssDemo/conicGradient.jsx
+ * @author: dongyang
+ */
+import React, { useEffect } from "react";
+
+import "./conicGradient.less";
+
+const ConicGradient = () => {
+  useEffect(() => {
+    const container = document.getElementById("g-container");
+    const img = document.getElementById("g-img");
+
+    container.addEventListener("mousemove", (event) => {
+      img.style.visibility = "visible";
+
+      const target = event.target;
+      const rect = target.getBoundingClientRect();
+
+      var offsetX = event.clientX - rect.left;
+      var offsetY = event.clientY - rect.top;
+
+      var percentX = (
+        Math.min(Math.max(offsetX / rect.width, 0), 1) * 100
+      ).toFixed(2);
+      var percentY = (
+        Math.min(Math.max(offsetY / rect.height, 0), 1) * 100
+      ).toFixed(2);
+
+      console.log("X: " + percentX + "%");
+      console.log("Y: " + percentY + "%");
+
+      container.setAttribute("style", `--x: ${percentX}%;--y: ${percentY}%;`);
+    });
+
+    container.addEventListener("mouseout", (event) => {
+      img.style.visibility = "hidden";
+    });
+  }, []);
+
+  return (
+    <div id="g-container">
+      <div id="g-filter"></div>
+      <div id="g-img"></div>
+    </div>
+  );
+};
+
+export default ConicGradient;
